25ad8cd8f38efb4458044b3452a534a416e594c2
Aline <aline.eikeland@gmail.com>
2020-05-12 13:10:51 +0200

    Refactor Controller
    be om framgång det här är en merge conflict serrni

--- IMat/src/iMat/iMatController.java
+++ IMat/src/iMat/iMatController.java
@@ -1,12 +1,17 @@
 
 package iMat;
 
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXScrollPane;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -22,11 +27,16 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import se.chalmers.cse.dat216.project.*;
 
+import javax.swing.plaf.basic.BasicOptionPaneUI;
+
 
 public class iMatController implements Initializable {
 
     IMatDataHandler db = IMatDataHandler.getInstance();
     iMATBackendController controller = new iMATBackendController();
+    iMatControllerBuyFrame buyFrameController = new iMatControllerBuyFrame(this);
+    static SkalProductCategory skalProductCategory = new SkalProductCategory();
+
 
     @FXML
     private AnchorPane product_description_pane;
@@ -47,66 +57,56 @@ public class iMatController implements Initializable {
     @FXML
     private ListView product_category_list;
 
+    @FXML private Label food_label;
+    @FXML private ImageView food_image;
+    @FXML private AnchorPane product_pane;
+    @FXML private AnchorPane product_description_pane;
+    @FXML private AnchorPane cart_pane;
+    @FXML private AnchorPane shoppingcart_page_pane;
+    @FXML private AnchorPane register_pane1;
+    @FXML private AnchorPane register_pane2;
+    @FXML private AnchorPane register_pane3;
+    @FXML private AnchorPane front_pane;
+    @FXML private AnchorPane root_pane;
+    @FXML private ListView product_category_list;
+    @FXML private ListView products;
+
+
+
+
+    private Map<String, MiniKlump> productListItemMap = new HashMap<String, MiniKlump>();
+
+
+
     @FXML
     private TextField search_field;
 
-    //tillhörande kassan
-    @FXML
-    private AnchorPane buyBetalningsplattaAnchorpane;
-    @FXML
-    private AnchorPane buyAndrabetalningsmetodPlatta;
-    @FXML
-    private AnchorPane buyAndraleveransPlatta;
-    @FXML
-    private AnchorPane buyAndratelefonnummerPlatta;
-    @FXML
-    private AnchorPane buyAndraadressPlatta;
-    @FXML
-    private JFXScrollPane buyKlumpScrollpane;
-    @FXML
-    private Label buy_idinvarukorg_label;
-    @FXML
-    private Label buy_sorteraefter_label;
-    @FXML
-    private Label buyAttbetalaLabel;
-    @FXML
-    private Label buyCurrentadressLabel;
-    @FXML
-    private Label buyCurrentbetalningsmetodLabel;
-    @FXML
-    private Label buyCurrentleveranstidLabel;
-    @FXML
-    private Label buyCurrenttelefonnummerLabel;
-    @FXML
-    private GridPane buyRowGridPane;
-    @FXML
-    private FlowPane buyKlumpFlowpane;
-    //behövs nog inte??
-    @FXML
-    private JFXButton buy_kategori_knapp;
-    @FXML
-    private JFXButton buy_pris_knapp;
-    @FXML
-    private JFXButton buy_inkopslista_knapp;
-    @FXML
-    private JFXButton buy_vidare_knapp;
-    @FXML
-    private JFXButton buyAndrabetalningsmetodButton;
-    @FXML
-    private JFXButton buyAndraLeveransmetod;
-    @FXML
-    private JFXButton buyAndraTelefonnummer;
-    @FXML
-    private JFXButton buyAndradressButton;
 
 
 
+    private void loadBetalning(){
 
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        initializeCheckOut();
 
+
+
+        //lyssnare till iMatControllerBuyFrame
+        buyFrameController.buy_vidare_knapp.onActionProperty().addListener(new ChangeListener<EventHandler<ActionEvent>>() {
+            @Override
+            public void changed(ObservableValue<? extends EventHandler<ActionEvent>> observableValue, EventHandler<ActionEvent> actionEventEventHandler, EventHandler<ActionEvent> t1) {
+                loadBetalning();
+            }
+        });
+        buyFrameController.buy_pris_knapp.onActionProperty().addListener(new ChangeListener<EventHandler<ActionEvent>>() {
+            @Override
+            public void changed(ObservableValue<? extends EventHandler<ActionEvent>> observableValue, EventHandler<ActionEvent> actionEventEventHandler, EventHandler<ActionEvent> t1) {
+                buyFrameController.populateByRow(sortByPris(), "pris");
+
+            }
+        });
 
 
     }
@@ -132,7 +132,9 @@ public class iMatController implements Initializable {
     public void openCartView() {
         cart_pane.toFront();
     }
-    //till checkout
+
+
+
     //sorterar lägsta pris med lägst index
     private ArrayList<ShoppingItem> sortByPris() {
         ArrayList<ShoppingItem> sortedList = new ArrayList<>();
@@ -157,83 +159,16 @@ public class iMatController implements Initializable {
 
 
 
-    private void populateByRow(ArrayList<ShoppingItem> list, String method){
-        buyKlumpScrollpane.getChildren().clear();
-        if (method == "kategori"){
-
-        }
-        //int rows = list.size()/2;  //avrundar uppåt???
-        for( ShoppingItem item : list){
-            buyKlumpFlowpane.getChildren().add(new MiniKlump(item, this));
-
-        }
-    }
-
-
-    public void populateCheckoutScrollPane(String method){
-        switch(method){
-            case "pris":
-                populateByRow(sortByPris(), method);
-                break;
-            case "inkopslista":
-                break;
-            case "kategori":
-                break;
-            default:
-               // throw Exception
-        }
-    }
-    //knappfunktioner
-
-    @FXML protected void sortPrisButton(){
-        populateCheckoutScrollPane("pris");
-    }
-
-    @FXML protected void sortInkopslistaButton(){
-        populateCheckoutScrollPane("inkopslista");
-    }
-
-    @FXML protected void sortKategoriButton(){
-        populateCheckoutScrollPane("kategori");
-    }
-
-    @FXML protected void VidareButton(){
-        initializeBetalning();
-    }
-
-    @FXML protected void changeAdressButton(){
-        buyAndraadressPlatta.toFront();
-    }
-    @FXML protected void changeBetalningsmetodButton(){
-        buyAndrabetalningsmetodPlatta.toFront();
-    }
-    @FXML protected void changeTelefonnummerButton(){
-        buyAndratelefonnummerPlatta.toFront();
-    }
-    @FXML protected void changeLeveransmetodButton(){
-        buyAndraleveransPlatta.toFront();
-    }
-
-    //init för betalning
-    public void initializeCheckOut(){
-        test.go(this);
-        //här: inled med att byta så att denna panen är längst fram alternativ ladda ny fxml
-        populateCheckoutScrollPane("kategori");
-        ;
-        //sätta etiketter:
-        Date date = new Date();
-        buy_idinvarukorg_label.setText("I din varukorg:");
-        buy_sorteraefter_label.setText("Sortera efter: ");
-        buyCurrentadressLabel.setText("Du bor på " + db.getCustomer().getAddress());
-        buyCurrentbetalningsmetodLabel.setText("");
-        buyCurrenttelefonnummerLabel.setText("Ditt telefonnummer är " + db.getCustomer().getMobilePhoneNumber());
-        buyCurrentleveranstidLabel.setText("Varorna kommer levereras den" + date.toString());
-        buyAttbetalaLabel.setText(String.valueOf(db.getShoppingCart().getTotal()));
-        //test
 
 
 
-    }
+
+
+
+
+
+
+
 
     private void initializeBetalning(){
 
